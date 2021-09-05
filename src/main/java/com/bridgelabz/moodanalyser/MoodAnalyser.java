09c232cf624e63f2b7fabbd5260ec3da4fecc327
prem-12345 <premkpatil1997@gmail.com>
2021-09-05 11:26:31 +0530

    Welcome to mood analyser program

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+
+    // Welcome to Mood Analyser program
+
+}
---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyserTest {
+
+
+
+}
