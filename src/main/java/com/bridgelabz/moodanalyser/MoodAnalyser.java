8c4a64ab0683764e6687edfe1f921365108e67dc
prem-12345 <premkpatil1997@gmail.com>
2021-09-05 11:42:34 +0530

    Added mood analyser method

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -2,6 +2,12 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
 
-    // Welcome to Mood Analyser program
+    public String analyseMood(String message){    /* Mood analyser method for happy and sad mood */
+        if(message.contains("happy")){
+            return "HAPPY";
+        }else {
+            return "SAD";
+        }
 
+    }
 }
--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,6 +1,26 @@
 package com.bridgelabz.moodanalyser;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MoodAnalyserTest {
+
+    /* This test case expected happy mood */
+    @Test
+    public void givenMoodMessage_contains_happy_shouldReturn_happy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in happy mood");
+        assertEquals("HAPPY",mood);
+    }
+
+    /* This test case expected sad mood */
+    @Test
+    public void givenMoodMessage_contains_sad_shouldReturn_sad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in sad mood");
+        assertEquals("SAD",mood);
+    }
 
 
 
